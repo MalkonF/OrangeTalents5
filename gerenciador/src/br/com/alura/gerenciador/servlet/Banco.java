0c7865743b29d8dbe3d5d0b85b6cd3fe0eedaf7e
anonymous <sudo777@protonmail.com>
2021-05-06 16:22:35 -0300

    Criado pagina para listar empresas cadastradas

--- gerenciador/src/br/com/alura/gerenciador/servlet/Banco.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/Banco.java
@@ -7,13 +7,21 @@ public class Banco {
 
 	private static List<Empresa> lista = new ArrayList<>();
 
-	public void adiciona(Empresa empresa) {
+	static {
+		Empresa empresa = new Empresa();
+		empresa.setNome("Alura");
+		Empresa empresa2 = new Empresa();
+		empresa2.setNome("Caelum");
 		lista.add(empresa);
+		lista.add(empresa2);
 
+	}
+
+	public void adiciona(Empresa empresa) {
+		Banco.lista.add(empresa);
 	}
 
 	public List<Empresa> getEmpresas() {
 		return Banco.lista;
 	}
-
 }
--- gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -18,15 +18,15 @@ public class NovaEmpresaServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-	
+
 		System.out.println("Cadastrando nova empresa");
 		String nomeDaEmpresa = request.getParameter("nome");
 		Empresa empresa = new Empresa();
 		empresa.setNome(nomeDaEmpresa);
-		
+
 		Banco banco = new Banco();
 		banco.adiciona(empresa);
-		
+
 		PrintWriter out = response.getWriter();
 		out.println("<html><body>Empresa " + nomeDaEmpresa + " cadastrada com sucesso!</body></html>");
 	}
