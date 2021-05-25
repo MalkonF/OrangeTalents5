1fdeedd7bb01d1375a09e45ead6458a40d30ddbd
MalkonF <sudo777@protonmail.com>
2021-05-25 12:44:52 -0300

    Inserindo novos testes para testar valores null // Usando before e after para iniciar conexao com bd, iniciar transação e fechar transação / .gitignore criado para ignorar certos arquivos de configuração

--- pm73-dao/test/br/com/caelum/pm73/dao/UsuarioDaoTest.java
+++ pm73-dao/test/br/com/caelum/pm73/dao/UsuarioDaoTest.java
@@ -1,14 +1,35 @@
 package br.com.caelum.pm73.dao;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import org.hibernate.Session;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import br.com.caelum.pm73.dominio.Usuario;
 
 //antes de executar este código rode a classe CriaTabelas para o hibernate criar as tabelas em memoria
 public class UsuarioDaoTest {
+
+	private Session session;
+	private UsuarioDao usuarioDao;
+
+	@Before
+	public void antes() {
+		// criamos a sessao e a passamos para o dao
+		session = new CriadorDeSessao().getSession();
+		usuarioDao = new UsuarioDao(session);
+		session.beginTransaction();
+	}
+
+	@After
+	public void depois() {
+		session.getTransaction().rollback();
+		// fechamos a sessao
+		session.close();
+	}
 
 	@Test
 	public void deveEncontrarPeloNomeEEmail() {
@@ -22,10 +43,20 @@ public class UsuarioDaoTest {
 
 		// agora buscamos no banco
 		Usuario usuarioDoBanco = usuarioDao.porNomeEEmail("João da Silva", "joao@dasilva.com.br");
+		Usuario usuarioDoBanco2 = usuarioDao.porNomeEEmail(null, null);
 
 		assertEquals("João da Silva", usuarioDoBanco.getNome());
 		assertEquals("joao@dasilva.com.br", usuarioDoBanco.getEmail());
+		assertNull("Maria Joaquina", usuarioDoBanco2);
+		assertNull("maria@email.com.br", usuarioDoBanco2);
 
-		session.close();
 	}
+
+	@Test
+	public void deveRetornarNuloSeNaoEncontrarUsuario() {
+		Usuario usuarioDoBanco = usuarioDao.porNomeEEmail("João Joaquim", "joao@joaquim.com.br");
+
+		assertNull(usuarioDoBanco);
+	}
+
 }
