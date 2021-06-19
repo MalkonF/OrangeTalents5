95fb3da77da92e87b832e99afd4ba0cff48c8b48
MalkonF <sudo777@protonmail.com>
2021-06-18 23:16:07 -0300

    Refatorando código parte do consumidor

--- ecommerce/src/main/java/br/com/alura/FraudDetectorService.java
+++ ecommerce/src/main/java/br/com/alura/FraudDetectorService.java
@@ -1,6 +1,7 @@
 package br.com.alura;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
@@ -12,46 +13,28 @@ import java.util.UUID;
 public class FraudDetectorService {
 
     public static void main(String[] args) {
-        //cria um consumidor
-        var consumer = new KafkaConsumer<String, String>(properties());
-        //tópico que vai escutar. Geralmente só escuta um tópico pq cada serviço vai ter um objetivo em específico
-        consumer.subscribe(Collections.singletonList("ECOMMERCE_NEW_ORDER"));
-        while (true) { //pergunta se tem mensagem no topico pelo tempo de 1000 segundos. Em records vai ta os registros que foram devolvidos
-            var records = consumer.poll(Duration.ofMillis(100));
-            if (!records.isEmpty()) {
-                System.out.println("Encontrei " + records.count() + " registros");
-                for (var record : records) { //p cada um dos registros imprime chave, valor da mensagem, partição da msg e offset
-                    System.out.println("------------------------------------------");
-                    System.out.println("Processing new order, checking for fraud");
-                    System.out.println(record.key());
-                    System.out.println(record.value());
-                    System.out.println(record.partition());
-                    System.out.println(record.offset());
-                    try {
-                        Thread.sleep(5000);//simula processamento de fraude
-                    } catch (InterruptedException e) {
-                        // ignoring
-                        e.printStackTrace();
-                    }
-                    System.out.println("Order processed");
-                }
-            }
-        }
+        var fraudService = new FraudDetectorService();
+        var service = new KafkaService(FraudDetectorService.class.getSimpleName(),
+                "ECOMMERCE_NEW_ORDER",
+                fraudService::parse);
+        service.run();
     }
 
-    private static Properties properties() {
-        var properties = new Properties();
-        //onde ele se conectar para escutar
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        // vai transformar de bytes para string. Quando ele produziu a mensagem na outra classe ele serializou foi de string para bytes
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        //cria um grupo para o service FraudDetectorService
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
-        //gera um CONSUMER_ID diferente a cada vez que roda esse .java
-        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, FraudDetectorService.class.getSimpleName() + "-" + UUID.randomUUID());
-        //qnd recebe a mensagem ele já avisa que já consumiu ela, não deixa acumular. Máximo de records que vai consumir por vez é 1
-        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1");
-        return properties;
+    private void parse(ConsumerRecord<String, String> record) {
+        System.out.println("------------------------------------------");
+        System.out.println("Processing new order, checking for fraud");
+        System.out.println(record.key());
+        System.out.println(record.value());
+        System.out.println(record.partition());
+        System.out.println(record.offset());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            // ignoring
+            e.printStackTrace();
+        }
+        System.out.println("Order processed");
     }
+
 }
+
